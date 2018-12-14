dfb1013f0b91184a26403ea00e6679e15b61435b
mapleFU <1506118561@qq.com>
2018-12-14 13:53:08 +0800

    [ADD] add submodule and config common data

--- src/main/java/cn/cloudbot/botmanager/receiver/receiverImpl/ReceiverImpl.java
+++ src/main/java/cn/cloudbot/botmanager/receiver/receiverImpl/ReceiverImpl.java
@@ -1,14 +1,14 @@
 package cn.cloudbot.botmanager.receiver.receiverImpl;
 
-import cn.cloudbot.botmanager.domain.message.post_event.StringRespMessage;
 import cn.cloudbot.botmanager.receiver.MessageReceiver;
+import cn.cloudbot.common.Message.ServiceMessage.RobotRecvMessage;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
 
 @EnableBinding(MessageReceiver.class)
 public class ReceiverImpl {
     @StreamListener(MessageReceiver.CHANNEL)
-    private void handleInput(StringRespMessage respMessage) {
+    private void handleInput(RobotRecvMessage respMessage) {
         System.out.println("收到数据: " + respMessage.toString());
     }
 }
